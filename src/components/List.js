1e704411d826664eccfe3a7ca01d24a1cb2e6c2a
Nícolas Alexandre <nicolas.xavier@cobasi.com.br>
2022-01-31 16:45:47 -0300

    iniciando css

--- src/components/List.js
+++ src/components/List.js
@@ -1,37 +1,21 @@
-import './css/styles.css';
+import { BsEraser, BsPlusSquare, BsTrash } from 'react-icons/bs';
+import './styles.css';
 
 function List() {
-
-  function cadastrar(e) {
-    e.preventDefault()
-    console.log('Cadastrou');
-  }
-
-  function meuEvento() {
-    console.log('clicou');
-  }
-
   return (
     <>
-      <div className="box1" id="heading">
-        <h1>Lista</h1>
+      <div className='box'>
+        <input className='boxInput' type="text" placeholder="Adicione um novo item" />
+        <BsPlusSquare />
+
         <div>
-          <form className="item" action="/" method="post" onSubmit={cadastrar}>
-            <input type="text" name="n" placeholder="Adicione um novo item" />
-            <button id="b" type="submit" value='cadastrar' onClick={meuEvento}><span id="s2"> + </span></button>
-          </form>
-        </div>
-        <div className="round">
-          <input type="checkbox" name="checkbox" value="<%= newListItem[i]._id%>" onChange="this.form.submit()" id="checkbox" />
-          <label for="checkbox"></label>
-        </div>
-      </div>
-      <div className="box">
-        <form action="/delete" method="post">
-          <div className="item">
+          <input type="checkbox" />
+          <div>
             <p></p>
+            <BsTrash />
+            <BsEraser />
           </div>
-        </form>
+        </div>
       </div>
     </>
   );
